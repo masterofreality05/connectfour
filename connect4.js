72871a9d72bfc2083842706bcddd2e2194872c03
SEAN WHITTY <SEANWHITTY92@gmail.com>
2023-02-01 20:38:20 +0100

    cleaned up and formatted the written code

--- connect4.js
+++ connect4.js
@@ -9,31 +9,28 @@ let HEIGHT = 6; //using let for both WIDTH and HEIGHT variables to use with Jasm
 let turnCounter = 1;
 let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
-
-/** makeBoard: create in-JS board structure:
+/* makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
-function add2(num){return num + 2}; //test function for Jasmine
-
 function makeBoard() {
 let row = []
-
 /*
 for(let y = 0; y < WIDTH; y++){
   row.push(0)
 }
-
 for(let x = 0; x < HEIGHT; x++){
   board.push(row)
+
+this code was passing the same subarr reference of "row" to each array element of board
+we declared let row at the start of the function, then added sub array elements
+and for each incrememntal value of WIDTH we pushed row over and over, the 'same array'
+now solved with a nested Loop. but why?
 */
-//this code was passing the same subarr reference of "row" to each array element of board
-//we declared let row at the start of the function, then added sub array elements
-//and for each incrememntal value of WIDTH we pushed row over and over, the 'same array'
-//now solved with a nested Loop. but why?
 for(let y = 0; y < HEIGHT; y++){
-  //for each incremental value of HEIGHT, we redeclare a new row (hopefully altering its reference)
-  //if we were to declare the row outside of a loop, we would not create a new one each time and therefore would reference the same
-  //each time, meaning if we were to reassignwe would update the same row element in every row. (which was my problem before)
+/*for each incremental value of HEIGHT, we redeclare a new row (hopefully altering its reference)
+if we were to declare the row outside of a loop, we would not create a new one each time and therefore would reference the same
+each time, meaning if we were to reassignwe would update the same row element in every row. (which was my problem before)
+  */
   let row = [];
   for(let x = 0; x < WIDTH; x++){
     //for now we loop through the iterable WIDTH on each row, and for each incremental value of WIDTH we push an empty array(cell)
@@ -46,27 +43,18 @@ for(let y = 0; y < HEIGHT; y++){
 }
 //then we return the board from our makeBoardFunction. 
   return board
-
-
 }
 
-
-
-/** makeHtmlBoard: make HTML table and row of column tops. */
-
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
-  const htmlBoard = document.getElementById('board');
-  //completed 
 
+  const htmlBoard = document.getElementById('board'); //completed
+ 
   // TODO: add comment for this code
   //comment added to handleClick which is implemented upon a clicked event listener
-   
   let top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
   
-
   for (let x = 0; x < WIDTH; x++) {
     const headCell = document.createElement("td");
     headCell.setAttribute("id", x);
@@ -97,11 +85,7 @@ function makeHtmlBoard() {
     //lets add each row to our html board (HTML element with board ID)
   }
 }
-
-/** findSpotForCol: given column x, return top empty y (null if filled) */
-
 function findSpotForCol(col,player) {
-  // TODO: write the real version of this, rather than always returning 0
   //x is vertical and y is horizontal indexed x-y
   //x will be passed in from the handleClick function x is the column, (horizontal index
   //console.log(x + "testing") when we click a column  top the x access value is being passed to this function.
@@ -114,7 +98,6 @@ function findSpotForCol(col,player) {
     if(board[col][0] !== 0){
       null
       
-
     } else {
     //we want to check its the furthest unchecked, or last vertical box.
     if (board[col][row + 1] !== 0 || row == HEIGHT -1){
@@ -124,8 +107,6 @@ function findSpotForCol(col,player) {
         //this is working nicely now, 
         //conditions will update array if the one after it is full, or its the last spot. 
         //updating our board array JS nice and good. 
-    
-
         board[col][row] = `${currPlayer}`;
         return row
 
@@ -133,88 +114,65 @@ function findSpotForCol(col,player) {
       null
     }
     }
-    
-  
   }
   //returning our x and y access in our board JS table. 
 }
-
-
 /** placeInTable: update DOM to place piece into HTML table of board */
-
 function placeInTable(col, row, color) {
   // TODO: make a div and insert into correct table cell
  try {
-  let selectedCell = document.getElementById(`${row}-${col}`)
+  let selectedCell = document.getElementById(`${row}-${col}`);
   selectedCell.style.backgroundColor = `${currPlayer}`;
  } catch (error) {
-  console.log('The column is fulL!')
-  
- } 
+  console.log('The column is fulL!'); 
+ };
 }
-
-/** endGame: announce game end */
-
 function endGame(msg) {
-  prompt(`${currPlayer} wins!`)
+  prompt(`${currPlayer} wins!`);
 }
-
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
   // get x from ID of clicked cell
   var x = +evt.target.id;
+  // switch players
+  // TODO: switch currPlayer 1 <-> 2 (completed)
   turnCounter++
-  turnCounter % 2 === 1? currPlayer = 'red': currPlayer = 'blue'
+  turnCounter % 2 === 1? currPlayer = 'red': currPlayer = 'blue';
   //a global variable (turnCounter) is incremented and determines if blue or red took the turn and loggs it to the console. 
   //this is also part of the 2 player turn logic.
-  console.log(`${currPlayer} just added a coin`)
-  //console.log(evt.target.getAttribute('id') + " is our target clicked")
+  console.log(`${currPlayer} just added a coin`);
   //with the get attribute method on our evt.target we can select the column number when clicked
   //if we pass this to our findSpotForCol function we can find the next available space for that particular column,
-  let columnSelected = evt.target.getAttribute('id')
-  console.log(columnSelected)
-  //console.log(findSpotForCol(columnSelected) + "is our returned amount from find spot")
-  placeInTable(columnSelected,findSpotForCol(columnSelected,currPlayer),currPlayer)
+
+  placeInTable(x,findSpotForCol(x,currPlayer),currPlayer)
   if(checkForWin() === true){
-    console.log("winnn")
     endGame()
   }
   //we are identifying now the column selected by its ID attribute 
   //we will pass the column index into our findSpotForCol function
   //placeInTable(findSpotForCol(columnSelected)[0], findSpotForCol(columnSelected)[1]);
-
   // get next spot in column (if none, ignore click)
   //var y = findSpotForCol(x);
   //if (y === null)
-
-
   }
-
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
-  
+  // check for tie (yet to do )
+  let checkForTie = () => {
+  }
+  // TODO: check if all cells in board are filled; if so call, call endGame
+/** checkForWin: check board cell-by-cell for "does a win start here?" */
 
-  // check for win
+ // check for win
   //if (checkForWin()) {
     //return endGame(`Player ${currPlayer} won!`);
   //}
-
-  // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
-
-  // switch players
-  // TODO: switch currPlayer 1 <-> 2
-//}
-
-/** checkForWin: check board cell-by-cell for "does a win start here?" */
-
 function checkForWin() {
   function _win(cells) {
     // Check four cells to see if they're all color of current player
     //  - cells: list of four (y, x) cells
     //  - returns true if all are legal coordinates & all match currPlayer
-
     return cells.every(
       ([y, x]) =>
         y >= 0 &&
@@ -224,7 +182,6 @@ function checkForWin() {
         board[y][x] === currPlayer
     );
   }
-
   /*TODO: read and understand this code. Add comments to help you.
   I understand the majority of what goes on here, and in the planning stage at the start of the exercise I thought 
    I would have to write it myself and here were my predictions, lets see how they compeare
@@ -246,7 +203,6 @@ if (array[0][i].hasAttribute('red') && array[1][i + 1].hasAttribute('red') && ar
 What I thought would be the case is that when we drop the coin we would have to account for vertical +1,+2,+3 and -1-2-3
 but it seems to not totally be the case, going to read and understand how this is working. 
    */
-
   for (var y = 0; y < HEIGHT; y++) {
     for (var x = 0; x < WIDTH; x++) {
       const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
@@ -260,7 +216,6 @@ but it seems to not totally be the case, going to read and understand how this i
       }
     }
   }
-
 
 }
 makeBoard();
